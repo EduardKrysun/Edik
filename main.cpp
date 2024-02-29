5a1f3dc51a0bf9cb151d2d4b63af00b0078e95a2
EduardKrysun <161637870+EduardKrysun@users.noreply.github.com>
2024-02-29 08:19:19 +0000

    New code

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
-#include <array>
 #include <algorithm>
 
-void swapMinMax(std::array<double, 10> &arr) { // Вказуємо розмір як 10, ви можете вибрати відповідний розмір
-    auto min_it = std::min_element(arr.begin(), arr.end());
-    auto max_it = std::max_element(arr.begin(), arr.end());
+void swapMinMax(double *arr, int size) {
+    if (size <= 0) // перевірка невідповідного розміру масиву
+        return;
+
+    double *min_it = std::min_element(arr, arr + size);
+    double *max_it = std::max_element(arr, arr + size);
     std::swap(*min_it, *max_it);
 }
 
 int main() {
-    const int n = 10; // Розмір масиву
-    std::array<double, n> arr;
+    int n;
+    std::cout << "Введіть розмір масиву: ";
+    std::cin >> n;
+
+    if (n <= 0) { // перевірка невідповідного розміру масиву
+        std::cerr << "Розмір масиву повинен бути більше 0." << std::endl;
+        return 1;
+    }
+
+    double *arr = new double[n];
 
     std::cout << "Введіть елементи масиву: ";
     for (int i = 0; i < n; i++) {
         std::cin >> arr[i];
     }
 
-    swapMinMax(arr);
+    swapMinMax(arr, n);
 
     std::cout << "Масив після заміни місцями найбільшого і найменшого елементів:\n";
     for (int i = 0; i < n; i++) {
@@ -25,4 +35,7 @@ int main() {
     }
     std::cout << std::endl;
 
+    delete[] arr; // важливо звільнити пам'ять
+
     return 0;
+}
